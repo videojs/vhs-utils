6d5bf929dcb9e39b5a35695bf3b6e71ad474d750
brandonocasey <brandonocasey@gmail.com>
2020-06-05 17:39:25 -0400

    fix content generation

--- generate-formats.sh
+++ generate-formats.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-DEBUG=1
+DEBUG=""
 
 if [ -n "$1" ]; then
-	DEBUG=0
+	DEBUG="foo"
 fi
 
 
@@ -11,16 +11,19 @@ JOBS=""
 
 __ffmpeg() {
 	for last; do true; done
-	cmd="ffmpeg -hide_banner -loglevel panic -y -i oceans.mp4 -t 00:00:05 $*"
+	cmd="ffmpeg -loglevel panic -y -i oceans.mp4 -strict experimental -hide_banner -t 00:00:01 $*"
 
-	bash -c "$cmd"
-	RETCODE=$?
-	if ! $RETCODE; then
-		if ! $DEBUG; then
+	if ! bash -c "$cmd"; then
+		if [ -z "$DEBUG" ]; then
 			echo "FAIL: $last"
 		else
 			echo "FAIL: $cmd"
 		fi
+	else
+		if [ -z "$DEBUG" ]; then
+			echo "PASS: $last"
+		fi
+
 	fi
 }
 
@@ -30,7 +33,7 @@ _ffmpeg() {
 }
 
 
-__x265() {
+__xTwoSixFive() {
 	container="$1"
 	name="$2"
 	args="$3"
@@ -41,17 +44,29 @@ __x265() {
 	else
 		retag=""
 	fi
+	cmd="ffmpeg -loglevel panic -y -i oceans.mp4 -strict experimental -hide_banner -t 00:00:01 -f yuv4mpegpipe -pix_fmt yuv420p - | x265 --log-level 0 --y4m --input - $args '${container}/${name}.265' && ffmpeg -hide_banner -loglevel panic -y -i '${container}/${name}.265' -strict experimental -c copy ${retag} '${container}/${name}.${container}'"
 
-	bash -c "ffmpeg -hide_banner -loglevel panic -y -i oceans.mp4 -t 00:00:05 -f yuv4mpegpipe -pix_fmt yuv420p - | x265 --y4m --input - $args '${container}/${name}.265' && ffmpeg -y -i '${container}/${name}.265' -c copy ${retag} '${container}/${name}.${container}' && rm -f '${container}/${name}.265'"
+	if ! bash -c "$cmd"; then
+		if [ -z "$DEBUG" ]; then
+			echo "FAIL: $last"
+		else
+			echo "FAIL: $cmd"
+		fi
+	else
+		if [ -z "$DEBUG" ]; then
+			echo "PASS: $last"
+		fi
+	fi
 
+	rm -f "${container}/${name}.265"
 }
 
-_x265() {
-	__x265 "$1" "$2" "$3" "$4" &
+_xtwoSixFive() {
+	__xTwoSixFive "$1" "$2" "$3" "$4" &
 	JOBS="${JOBS} ${!}"
 }
 
-for container in mkv avi ogg mp4 webm ts wav; do
+for container in mkv; do
 	rm -rf "./${container}/"
 	mkdir -p "./${container}"
 	# audio formats
@@ -60,7 +75,7 @@ for container in mkv avi ogg mp4 webm ts wav; do
 	_ffmpeg -vn -acodec aac -profile:a aac_he_v2 "${container}/mp4a.40.29.${container}"
 	_ffmpeg -vn -acodec mp3 "${container}/mp4a.40.34.${container}"
 	_ffmpeg -vn -acodec speex "${container}/speex.${container}"
-	_ffmpeg -vn -acodec opus "${container}/opus.${container}"
+	_ffmpeg -vn -acodec libopus "${container}/opus.${container}"
 	_ffmpeg -vn -acodec vorbis "${container}/vorbis.${container}"
 	_ffmpeg -vn -acodec ac3 "${container}/ac3.${container}"
 	_ffmpeg -vn -acodec eac3 "${container}/ec3.${container}"
@@ -70,7 +85,7 @@ for container in mkv avi ogg mp4 webm ts wav; do
 	# video formats
 	# TODO: generate more content
 	# Profile.leveltier.bitdepth.[monochromeflag].[chromesubsample].[colorprimary].[transferchar].[matrixco].[full color]
-	_ffmpeg -an -vcodec av1 -strict experimental -cpu-used 8 "${container}/av01.${container}"
+	_ffmpeg -an -vcodec av1 -cpu-used 8 "${container}/av01.${container}"
 
 	# TODO: us another encoder, ffmpeg does not support codecPrivate for vp09
 	# profile.level.depth.chroma.[color-primary].[transferchar].[matrixco].[blacklevel]
@@ -87,17 +102,17 @@ for container in mkv avi ogg mp4 webm ts wav; do
 	_ffmpeg -an -vcodec mpeg4 -profile:v 0 -level 9 "${container}/mp4v.20.9.${container}"
 	_ffmpeg -an -vcodec mpeg4 -profile:v 15 -level 0 "${container}/mp4v.20.240.${container}"
 
-	_x265 "${container}" "hev1.4.10.H120.99.88" "--profile main12 --level-idc 4.0"
+	_xtwoSixFive "${container}" "hev1.4.10.H120.99.88" "--profile main12 --level-idc 4.0"
 	# profile change
-	_x265 "${container}" "hev1.2.4.H120.90" "--profile main10 --level-idc 4.0"
+	_xtwoSixFive "${container}" "hev1.2.4.H120.90" "--profile main10 --level-idc 4.0"
 	# level change
-	_x256 "${container}" "hev1.4.10.H150.99.88" "--profile main12 --level-idc 5.0"
+	_xtwoSixFive "${container}" "hev1.4.10.H150.99.88" "--profile main12 --level-idc 5.0"
 	# no tier
-	_x256 "${container}" "hev1.4.10.L120.99.88" "--profile main12 --level-idc 4.0 --no-high-tier"
+	_xtwoSixFive "${container}" "hev1.4.10.L120.99.88" "--profile main12 --level-idc 4.0 --no-high-tier"
 	# another profile
-	_x256 "${container}" "hev1.1.6.H120.90" "--profile main --level-idc 4.0"
+	_xtwoSixFive "${container}" "hev1.1.6.H120.90" "--profile main --level-idc 4.0"
 	# other codec tag
-	_x265 "${container}" "hvc1.4.10.H120.99.88" "--profile main12 --level-idc 4.0" "1"
+	_xtwoSixFive "${container}" "hvc1.4.10.H120.99.88" "--profile main12 --level-idc 4.0" "1"
 
 	_ffmpeg -an -vcodec theora "${container}/theora.${container}"
 done;
@@ -112,14 +127,13 @@ _ffmpeg -vn -acodec mp3 "audio/mp4a.40.34.mp3"
 _ffmpeg -vn -acodec speex "audio/speex.spx"
 _ffmpeg -vn -acodec opus "audio/opus.opus"
 _ffmpeg -vn -acodec ac3 "audio/ac3.ac3"
-_ffmpeg -vn -acodec ec3 "audio/ec3.ec3"
 _ffmpeg -vn -acodec flac "audio/flac.flac"
 
 _ffmpeg -vn -acodec wav "audio/wav.wav"
 _ffmpeg -vn -acodec aac "audio/aac.wav"
 _ffmpeg -vn -acodec mp3 "audio/mp3.wav"
 
-_ffmpeg -an -vcodec libx264 "video/x264.264" &
+_ffmpeg -an -vcodec libx264 "video/x264.264"
 
 wait
 # remove any failures
