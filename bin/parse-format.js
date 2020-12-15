f670fe3aa5b237db06adb7246a4d0792831e662d
brandonocasey <brandonocasey@gmail.com>
2020-12-15 16:36:31 -0500

    allow pipe to parse-format

--- bin/parse-format.js
+++ bin/parse-format.js
@@ -9,6 +9,8 @@ const path = require('path');
 const showHelp = function() {
   console.log(`
   parse-format [...media-files]
+  curl 'some-media-ulr' | parse-format
+  wget -O - -o /dev/null 'some-media-url' | parse-format
 
   parse containers and codecs given a media file that contains that information.
 
@@ -18,7 +20,7 @@ const showHelp = function() {
 };
 
 const parseArgs = function(args) {
-  const options = {};
+  const options = {files: []};
 
   for (let i = 0; i < args.length; i++) {
     const arg = args[i];
@@ -30,7 +32,6 @@ const parseArgs = function(args) {
       showHelp();
       process.exit(0);
     } else {
-      options.files = options.files || [];
       options.files.push(arg);
     }
   }
@@ -38,44 +39,86 @@ const parseArgs = function(args) {
   return options;
 };
 
-const options = parseArgs(process.argv.slice(2));
+const cli = function(stdin) {
+  const options = parseArgs(process.argv.slice(2));
+  const promises = [];
 
-console.log();
+  if (stdin) {
+    const p = new Promise(function(resolve, reject) {
+      let allData;
+      let lastResult;
 
-Promise.all(options.files.map(function(file) {
-  return new Promise(function(resolve, reject) {
-    const stream = fs.createReadStream(path.resolve(file));
-    let allData;
-    let lastResult;
+      // read from stdin, aka piped input
+      stdin.on('readable', () => {
+        let chunk;
 
-    stream.on('data', (chunk) => {
-      allData = concatTypedArrays(allData, chunk);
+        // Use a loop to make sure we read all available data.
+        while ((chunk = process.stdin.read()) !== null) {
+          allData = concatTypedArrays(allData, chunk);
+        }
 
-      lastResult = parseFormatForBytes(allData);
+        lastResult = parseFormatForBytes(allData);
 
-      if (!Object.keys(lastResult.codecs).length) {
-        return;
-      }
+        if (!Object.keys(lastResult.codecs).length) {
+          return;
+        }
+      });
 
-      stream.destroy();
+      stdin.on('end', () => {
+        console.log('Results for stdin');
+        console.log(lastResult);
+        if (!Object.keys(lastResult.codecs).length) {
+          console.warn('WARNING no codecs found');
+        }
+        console.log();
+        resolve();
+      });
     });
-    stream.on('error', reject);
 
-    stream.on('close', () => {
-      console.log(`Results for ${file}`);
-      console.log(lastResult);
-      if (!Object.keys(lastResult.codecs).length) {
-        console.warn('WARNING no codecs found');
-      }
-      console.log();
-      resolve();
+    promises.push(p);
+  }
+
+  options.files.forEach(function(file) {
+    const p = new Promise(function(resolve, reject) {
+      const stream = fs.createReadStream(path.resolve(file));
+      let allData;
+      let lastResult;
+
+      stream.on('data', (chunk) => {
+        allData = concatTypedArrays(allData, chunk);
+
+        lastResult = parseFormatForBytes(allData);
+
+        if (!Object.keys(lastResult.codecs).length) {
+          return;
+        }
+
+        stream.destroy();
+      });
+      stream.on('error', reject);
+
+      stream.on('close', () => {
+        console.log(`Results for ${file}`);
+        console.log(lastResult);
+        if (!Object.keys(lastResult.codecs).length) {
+          console.warn('WARNING no codecs found');
+        }
+        console.log();
+        resolve();
+      });
     });
+
+    promises.push(p);
   });
-})).then(function() {
-  console.log('All files read!');
-  console.log();
-  process.exit(0);
-}).catch(function(e) {
-  console.error(e);
-  process.exit(1);
-});
+  return Promise.all(promises).then(function() {
+    console.log('All files read!');
+    console.log();
+    process.exit(0);
+  }).catch(function(e) {
+    console.error(e);
+    process.exit(1);
+  });
+};
+
+// no stdin if isTTY is set
+cli(!process.stdin.isTTY ? process.stdin : null);
