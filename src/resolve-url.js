ea098cf3b24586ad186850d589e17fb5450ec155
Gary Katsevman <git@gkatsev.com>
2021-02-23 17:46:32 -0500

    better feature detection for IE11

--- src/resolve-url.js
+++ src/resolve-url.js
@@ -7,14 +7,18 @@ const resolveUrl = (baseUrl, relativeUrl) => {
     return relativeUrl;
   }
 
+  // IE11 supports URL but not the URL constructor
+  // feature detect the behavior we want
+  const nativeURL = typeof window.URL === 'function';
+
   // if the base URL is relative then combine with the current location
-  if (window.URL) {
+  if (nativeURL) {
     baseUrl = new window.URL(baseUrl, window.location);
   } else if (!(/\/\//i).test(baseUrl)) {
     baseUrl = URLToolkit.buildAbsoluteURL(window.location && window.location.href || '', baseUrl);
   }
 
-  if (window.URL) {
+  if (nativeURL) {
     return new URL(relativeUrl, baseUrl).href;
   }
   return URLToolkit.buildAbsoluteURL(baseUrl, relativeUrl);
