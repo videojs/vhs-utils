64dc933a095ff76f83cd64e1dd83b252bed34d7a
Gary Katsevman <git@gkatsev.com>
2021-03-23 18:29:32 -0400

    bring back slice off protocol if we shouldn't have it

--- src/resolve-url.js
+++ src/resolve-url.js
@@ -11,6 +11,8 @@ const resolveUrl = (baseUrl, relativeUrl) => {
   // feature detect the behavior we want
   const nativeURL = typeof window.URL === 'function';
 
+  const protocolLess = !(/^https?:/.test(baseUrl));
+
   // if the base URL is relative then combine with the current location
   if (nativeURL) {
     baseUrl = new window.URL(baseUrl, window.location);
@@ -19,7 +21,14 @@ const resolveUrl = (baseUrl, relativeUrl) => {
   }
 
   if (nativeURL) {
-    return new URL(relativeUrl, baseUrl).href;
+    const newUrl = new URL(relativeUrl, baseUrl);
+
+    // if we're a protocol-less url, return a protocol-less url
+    if (protocolLess) {
+      return newUrl.href.slice(newUrl.protocol.length);
+    }
+
+    return newUrl;
   }
   return URLToolkit.buildAbsoluteURL(baseUrl, relativeUrl);
 
