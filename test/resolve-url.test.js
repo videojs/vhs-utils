9b10245b0b659b7a0297a0216c94779ee24ba709
Gary Katsevman <gary.katsevman@gmail.com>
2021-05-20 14:34:14 -0400

    fix: properly handle data URIs (#27)
    There are the following two cases:
    
        a data URI is provided for the baseURL
            in this case, ignore the data URI and resolve against window.location or a default location
        a data URI is provided for the relativeURL
            in this case, return the data URI as it should be treated as an absolute URL
    
    Fixes videojs/video.js#7240

--- test/resolve-url.test.js
+++ test/resolve-url.test.js
@@ -8,9 +8,11 @@ QUnit.module('URL resolver');
 
 QUnit.test('works with a selection of valid urls', function(assert) {
   let currentLocation = '';
+  let currentPath = '';
 
   if (window.location && window.location.protocol) {
     currentLocation = window.location.protocol + '//' + window.location.host;
+    currentPath = window.location.pathname.split('/').slice(0, -1).join('/');
   }
 
   assert.equal(
@@ -25,4 +27,23 @@ QUnit.test('works with a selection of valid urls', function(assert) {
   );
   assert.equal(resolveUrl('/a/b/cd/e.m3u8', 'z.ts'), currentLocation + '/a/b/cd/z.ts');
   assert.equal(resolveUrl('/a/b/cd/e.m3u8', '../../../z.ts'), currentLocation + '/z.ts');
+
+  assert.equal(
+    resolveUrl('data:application/dash+xml;charset=utf-8,http%3A%2F%2Fexample.com', 'hello.m3u8'),
+    // we need to add the currentPath because we're actually working relative to window.location
+    currentLocation + currentPath + '/hello.m3u8',
+    'resolves urls relative to window when given a data base url'
+  );
+
+  assert.equal(
+    resolveUrl('data:application/dash+xml;charset=utf-8,http%3A%2F%2Fexample.com', 'http://example.com/hello.m3u8'),
+    'http://example.com/hello.m3u8',
+    'absolute urls should still be absolute even with a data uri'
+  );
+
+  assert.equal(
+    resolveUrl('http://example.com', 'data:application/dash+xml;charset=utf-8,http%3A%2F%2Fexample.com'),
+    'data:application/dash+xml;charset=utf-8,http%3A%2F%2Fexample.com',
+    'data uri is treated as an absolute url'
+  );
 });
